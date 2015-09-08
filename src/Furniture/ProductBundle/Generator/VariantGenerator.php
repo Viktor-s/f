20222c9e4b67d3c9e904c40e5ac9d1b218d38df5
dvcdsys <dvcdsys@gmail.com>
2015-09-08 19:58:41 +0300

    Fix sku generator

--- src/Furniture/ProductBundle/Generator/VariantGenerator.php
+++ src/Furniture/ProductBundle/Generator/VariantGenerator.php
@@ -11,89 +11,103 @@ use Furniture\ProductBundle\Entity\Product;
 use Sylius\Component\Variation\Model\VariantInterface;
 use Symfony\Component\Validator\ValidatorInterface;
 
-
 class VariantGenerator extends ContainerAware {
-    
+
     protected $setBuilder;
-    
     protected $eventDispatcher;
-    
     protected $variantRepository;
-    
+
     function __construct(RepositoryInterface $variantRepository, SetBuilderInterface $setBuilder, ValidatorInterface $validator, EventDispatcherInterface $eventDispatcher) {
         $this->variantRepository = $variantRepository;
         $this->setBuilder = $setBuilder;
         $this->eventDispatcher = $eventDispatcher;
         $this->validator = $validator;
     }
-    
+
     /**
      * 
      * @param \Furniture\ProductBundle\Entity\Product $product
      */
-    public function generate( Product $product ) {
-        
-        if( !$product->hasOptions() && !$product->hasSkuOptionVariants() ){
+    public function generate(Product $product) {
+
+        if (!$product->hasOptions() && !$product->hasSkuOptionVariants()) {
             throw new \InvalidArgumentException('Cannot generate variants for an product without options and sku options');
         }
-        
+
         $optionSet = array();
         $optionMap = array();
-        
+
         /*
          * Get product option variants
          */
         $pOptionPref = 'o';
         foreach ($product->getOptions() as $k => $option) {
             foreach ($option->getValues() as $value) {
-                $optionSet[$pOptionPref.'_'.$k][] = $value->getId();
-                $optionMap[$pOptionPref.'_'.$value->getId()] = $value;
+                $route_id = $pOptionPref . '_' . $value->getId();
+                $optionSet[$pOptionPref . '_' . $k][] = $route_id;
+                $optionMap[$route_id] = $value;
             }
         }
-        
+
         /*
          * Get product sku option
          */
         $sOptionPref = 'so';
-        foreach($product->getSkuOptionVariantsGrouped() as $k => $option){
+        foreach ($product->getSkuOptionVariantsGrouped() as $k => $option) {
             foreach ($option as $value) {
-                $optionSet[$sOptionPref.'_'.$k][] = $value->getId();
-                $optionMap[$sOptionPref.'_'.$value->getId()] = $value;
+                $route_id = $sOptionPref . '_' . $value->getId();
+                $optionSet[$sOptionPref . '_' . $k][] = $route_id;
+                $optionMap[$route_id] = $value;
             }
         }
-        
-        //echo '<pre>'.print_r($optionSet,true).'</pre>';
-        
+
+        //echo '<pre>' . print_r($optionSet, true) . '</pre>';
+
         $permutations = $this->setBuilder->build($optionSet);
+
+        //echo '<pre>' . print_r($permutations, true) . '</pre>';
         foreach ($permutations as $permutation) {
             $variant = $this->variantRepository->createNew();
             $variant->setObject($product);
             $variant->setDefaults($product->getMasterVariant());
             //echo '<pre>'.print_r($permutation,true).'</pre>';
-            foreach ($permutation as $map_route => $id) {
+            if (!is_array($permutation)) {
+                $route_id = $permutation;
                 /*
                  * add to product option
                  */
-                if(split('_', $map_route)[0] == $pOptionPref ){
-                    $variant->addOption($optionMap[$pOptionPref.'_'.$id]);
+                if (split('_', $route_id)[0] == $pOptionPref) {
+                    $variant->addOption($optionMap[$route_id]);
                 }
-                                /*
+                /*
                  * add to sku product option
                  */
-                if(split('_', $map_route)[0] == $sOptionPref ){
-                    $variant->addSkuOption($optionMap[$sOptionPref.'_'.$id]);
+                if (split('_', $route_id)[0] == $sOptionPref) {
+                    $variant->addSkuOption($optionMap[$route_id]);
                 }
-            }
+            } else
+                foreach ($permutation as $k => $route_id) {
+                    /*
+                     * add to product option
+                     */
+                    if (split('_', $route_id)[0] == $pOptionPref) {
+                        $variant->addOption($optionMap[$route_id]);
+                    }
+                    /*
+                     * add to sku product option
+                     */
+                    if (split('_', $route_id)[0] == $sOptionPref) {
+                        $variant->addSkuOption($optionMap[$route_id]);
+                    }
+                }
             $product->addVariant($variant);
         }
         //die();
         $this->process($product, $variant);
     }
-    
-    public function process(Product $variable, VariantInterface $variant)
-    {
+
+    public function process(Product $variable, VariantInterface $variant) {
         $this->eventDispatcher->dispatch('sylius.variant.pre_create', new GenericEvent($variant));
     }
 
 }
-
