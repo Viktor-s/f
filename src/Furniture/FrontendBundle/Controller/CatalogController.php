b0f106e83ad6cf12c4129b1b84ed93b85c7d7993
dvcdsys <dvcdsys@gmail.com>
2015-09-28 08:28:06 +0300

    Catalog category selector

--- src/Furniture/FrontendBundle/Controller/CatalogController.php
+++ src/Furniture/FrontendBundle/Controller/CatalogController.php
@@ -12,9 +12,8 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 
-class CatalogController 
-{
-    
+class CatalogController {
+
     /**
      * @var \Twig_Environment
      */
@@ -40,20 +39,19 @@ class CatalogController
      * @var \Sylius\Bundle\TaxonomyBundle\Doctrine\ORM\TaxonRepository
      */
     private $taxonRepository;
-    
+
     /**
      *
      * @var \Sylius\Bundle\TaxonomyBundle\Doctrine\ORM\TaxonomyRepository
      */
     private $taxonomyRepository;
-    
+
     /**
      * @var TokenStorageInterface
      */
     private $tokenStorage;
-
     private $paginator;
-    
+
     /**
      * Construct
      *
@@ -64,13 +62,7 @@ class CatalogController
      * @param TokenStorageInterface       $tokenStorage
      */
     public function __construct(
-        \Twig_Environment $twig,
-        \Doctrine\ORM\EntityRepository $productRepository,
-        \Sylius\Bundle\TaxonomyBundle\Doctrine\ORM\TaxonRepository $taxonRepository,
-        EntityRepository $taxonomyRepository,
-        SpecificationRepository $specificationRepository,
-        SpecificationItemRepository $specificationItemRepository,
-        TokenStorageInterface $tokenStorage
+    \Twig_Environment $twig, \Doctrine\ORM\EntityRepository $productRepository, \Sylius\Bundle\TaxonomyBundle\Doctrine\ORM\TaxonRepository $taxonRepository, EntityRepository $taxonomyRepository, SpecificationRepository $specificationRepository, SpecificationItemRepository $specificationItemRepository, TokenStorageInterface $tokenStorage
     ) {
         $this->twig = $twig;
         $this->productRepository = $productRepository;
@@ -80,65 +72,62 @@ class CatalogController
         $this->specificationItemRepository = $specificationItemRepository;
         $this->tokenStorage = $tokenStorage;
     }
-    
-    public function taxonomy(Request $request){
-        $user = $this->tokenStorage->getToken()
-            ->getUser();
-        
-        $category = $this->taxonomyRepository->findOneBy(['name' => 'Category']);
-        
-        $content = $this->twig->render('FrontendBundle:Catalog:taxonomy.html.twig', [
-            'Category' => $category,
-        ]);
 
-        return new Response($content);
-    }
-    
-    protected function getAllChildTaxons($taxon)
-    {
+    protected function getAllChildTaxons($taxon) {
         $taxons = [];
-        foreach($taxon->getChildren() as $child){
+        foreach ($taxon->getChildren() as $child) {
             $taxons[] = $child;
             $taxons = array_merge($taxons, $this->getAllChildTaxons($child));
         }
         return $taxons;
     }
     
-    public function products(Request $request, $category_permalink)
-    {
+    protected function getCategoryTaxonomy(){
+        return $this->taxonomyRepository->findOneBy(['name' => 'Category']);
+    }
+
+
+    public function taxonomy(Request $request) {
         $user = $this->tokenStorage->getToken()
-            ->getUser();
-        
+                ->getUser();
+
+        $category = $this->getCategoryTaxonomy();
+
+        $content = $this->twig->render('FrontendBundle:Catalog:taxonomy.html.twig', [
+            'Category' => $category,
+        ]);
+
+        return new Response($content);
+    }
+
+    public function products(Request $request, $category_permalink) {
+        $user = $this->tokenStorage->getToken()
+                ->getUser();
+
         $taxon = $this->taxonRepository
                 ->findOneBy(['permalink' => $category_permalink]);
-        
+
         $child_taxons = $this->getAllChildTaxons($taxon);
-        
+
         $page = $request->get('page', 1);
-        
+
         $qBuilder = $this->productRepository->createQueryBuilder('p');
-        
-        //if($taxons = $request->get('taxons', false)){
-          //  $taxons = array_map( function($v){ return (int)$v; }, $taxons);
-            
-            $qBuilder->innerJoin('p.taxons', 'taxon')
+
+        $qBuilder->innerJoin('p.taxons', 'taxon')
                 ->andWhere('taxon in ( :taxons )')
                 ->setParameter('taxons', $child_taxons)
-                ;
-            
-       // }
-        
+        ;
+
         $products = $this->productRepository->getPaginator($qBuilder);
         
         $products->setMaxPerPage(12);
         $products->setCurrentPage($page);
         $content = $this->twig->render('FrontendBundle:Catalog:products.html.twig', [
             'products' => $products,
+            'category' => $this->getCategoryTaxonomy(),
         ]);
 
         return new Response($content);
-        
     }
-    
-}
 
+}
