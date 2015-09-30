8df5a4f141284ed202a6cecc39e65b157ee1ad89
Vitaliy Zhuk <zhuk2205@gmail.com>
2015-09-30 17:45:27 +0300

    Add delete all and control deletable system

--- src/Furniture/ProductBundle/Controller/VariantController.php
+++ src/Furniture/ProductBundle/Controller/VariantController.php
@@ -4,11 +4,83 @@ namespace Furniture\ProductBundle\Controller;
 
 use Sylius\Bundle\ProductBundle\Controller\VariantController as BaseVariantController;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class VariantController extends BaseVariantController {
-    
+class VariantController extends BaseVariantController
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function deleteAction(Request $request)
+    {
+        $hardDelete = $request->get('hard');
+        $em = $this->get('doctrine.orm.default_entity_manager');
+
+        if ($hardDelete) {
+            $em->getFilters()->disable('softdeleteable');
+        }
+
+        $response = parent::deleteAction($request);
+
+        if ($hardDelete) {
+            $em->getFilters()->enable('softdeleteable');
+        }
+
+        if ($request->get('_from')) {
+            return new RedirectResponse($request->get('_from'));
+        }
+
+        return $response;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function restoreAction(Request $request)
+    {
+        $response = parent::restoreAction($request);
+
+        if ($request->get('_from')) {
+            return new RedirectResponse($request->get('_from'));
+        }
+
+        return $response;
+    }
+
+    /**
+     * Clear deleted variants
+     *
+     * @param Request $request
+     * @param int     $productId
+     *
+     * @return RedirectResponse
+     */
+    public function clearDeletedAction(Request $request, $productId)
+    {
+        $em = $this->get('doctrine.orm.default_entity_manager');
+        $em->getFilters()->disable('softdeleteable');
+
+        /** @var \Furniture\ProductBundle\Entity\Product $product */
+        $product = $this->findProductOr404($productId);
+        $deletedVariants = $product->getDeletedVariants();
+
+        foreach ($deletedVariants as $variant) {
+            $em->remove($variant);
+        }
+
+        $em->flush();
+
+        $em->getFilters()->enable('softdeleteable');
+
+        if ($request->get('_from')) {
+            return new RedirectResponse($request->get('_from'));
+        }
+
+        return $this->redirectHandler->redirectToIndex();
+    }
+
     /**
      * Get variant generator.
      *
@@ -21,6 +93,10 @@ class VariantController extends BaseVariantController {
     
     /**
      * Generate all possible variants for given product id.
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function generateAction(Request $request)
     {
@@ -28,6 +104,7 @@ class VariantController extends BaseVariantController {
             throw new NotFoundHttpException('No product given.');
         }
 
+        /** @var \Furniture\ProductBundle\Entity\Product $product */
         $product = $this->findProductOr404($productId);
         $this->getGenerator()->generate($product);
 
