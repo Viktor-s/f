df0ffc44573ed76b909effbc040ed00b08d1cafa
Vitaliy Zhuk <zhuk2205@gmail.com>
2015-10-22 18:07:34 +0300

    Add memcached session handler

--- vagrant/manifests/10-php.pp
+++ vagrant/manifests/10-php.pp
@@ -5,6 +5,8 @@ augeas { 'php_config':
         'set date.timezone UTC',
         'set memory_limit 512M',
         'set max_execution_time 3600',
-        'set short_open_tag off'
+        'set short_open_tag off',
+        'set session.save_handler memcached',
+        'set session.save_path localhost:11211'
     ]
 }
