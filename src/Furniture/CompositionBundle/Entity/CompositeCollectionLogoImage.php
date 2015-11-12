fbb6bd58f0cbc43dce93081345e599054ffdb4aa
Vitaliy Zhuk <zhuk2205@gmail.com>
2015-11-12 19:26:03 +0200

    Add backend image for composite logo

--- src/Furniture/CompositionBundle/Entity/CompositeCollectionLogoImage.php
+++ src/Furniture/CompositionBundle/Entity/CompositeCollectionLogoImage.php
@@ -2,37 +2,36 @@
 namespace Furniture\CompositionBundle\Entity;
 
 use Sylius\Component\Core\Model\Image;
-use Furniture\CompositionBundle\Entity\CompositeCollection;
 use Furniture\CommonBundle\Uploadable\UploadableInterface;
 
 class CompositeCollectionLogoImage extends Image implements UploadableInterface
 {
-    
     /**
-     *
      * @var CompositeCollection
      */
     private $compositeCollection;
-    
+
     /**
-     * 
-     * @return \Furniture\CompositionBundle\Entity\CompositeCollection
+     * Get composite collection
+     *
+     * @return CompositeCollection
      */
     public function getCompositeCollection()
     {
         return $this->compositeCollection;
     }
-    
+
     /**
-     * 
-     * @param \Furniture\CompositionBundle\Entity\CompositeCollection $compositeCollection
-     * @return \Furniture\CompositionBundle\Entity\CompositeCollectionLogoImage
+     * Set composite collection
+     *
+     * @param CompositeCollection $compositeCollection
+     *
+     * @return CompositeCollectionLogoImage
      */
     public function setCompositeCollection(CompositeCollection $compositeCollection)
     {
         $this->compositeCollection = $compositeCollection;
+
         return $this;
     }
-    
 }
-
