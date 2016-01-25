dd408f705b3cd93176f7e229174645dbe41c00f5
Vitaliy Zhuk <zhuk2205@gmail.com>
2016-01-25 10:18:53 +0200

    Add reset password form type

--- src/Furniture/FrontendBundle/Form/Type/UserResetPasswordType.php
+++ src/Furniture/FrontendBundle/Form/Type/UserResetPasswordType.php
@@ -3,7 +3,29 @@
 namespace Furniture\UserBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class UserResetPasswordType extends AbstractType
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', 'email', [
+                'label' => 'Email',
+                'constraints' => [
+                    new Assert\Email()
+                ]
+            ]);
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'security_reset_password';
+    }
 }
