373b2488562935a3337d09daf0c77a5f264f429a
Vitaliy Zhuk <zhuk2205@gmail.com>
2016-02-22 17:09:19 +0200

    #426 Fix validator

--- src/Furniture/ProductBundle/Validator/Constraint/ProductVariantValidator.php
+++ src/Furniture/ProductBundle/Validator/Constraint/ProductVariantValidator.php
@@ -6,31 +6,33 @@ use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Constraint;
 use Furniture\ProductBundle\Entity\ProductVariant as ProductVariantEntity;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 
-class ProductVariantValidator extends ConstraintValidator {
+class ProductVariantValidator extends ConstraintValidator
+{
 
     /**
      * {@inheritDoc}
      */
-    public function validate($value, Constraint $constraint) {
+    public function validate($value, Constraint $constraint)
+    {
         if (!$value instanceof ProductVariantEntity) {
-            throw new UnexpectedTypeException($value, VariantInterface::class);
+            throw new UnexpectedTypeException($value, ProductVariantEntity::class);
         }
 
         if ($value->isMaster()) {
             return;
         }
 
-        if ($value->getProduct()->isSchematicProductType()) {
-
+        if ($value->getProduct()->isSchematicProductType() && $value->getProductScheme()) {
             $schemeParts = $value->getProductScheme()->getProductParts();
-            $variantParts = new ArrayCollection;
+            $variantParts = new ArrayCollection();
 
             foreach ($value->getProductPartVariantSelections() as $index => $ppvSelection) {
                 $ppvSelectionProductPart = $ppvSelection->getProductPart();
                 if (!$schemeParts->contains($ppvSelectionProductPart)) {
                     $this->context->addViolationAt(
-                            'productPartVariantSelections[' . $index . ']', $constraint->message, array(), null
+                        'productPartVariantSelections[' . $index . ']', $constraint->message, array(), null
                     );
                 } else {
                     if (!$variantParts->contains($ppvSelection->getProductPart())) {
@@ -44,7 +46,7 @@ class ProductVariantValidator extends ConstraintValidator {
             foreach ($schemeParts as $schemePart) {
                 if (!$variantParts->contains($schemePart)) {
                     $this->context->addViolationAt(
-                            'productPartVariantSelections', $constraint->message, array(), null
+                        'productPartVariantSelections', $constraint->message, array(), null
                     );
                 }
             }
