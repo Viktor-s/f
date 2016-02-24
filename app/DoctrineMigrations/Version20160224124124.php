fc156b09a4242cd65b2bb18204d052fbcd035b7e
dvcdsys <dvcdsys@gmail.com>
2016-02-24 12:44:05 +0200

    Add factory code to ProductVariant and ProductVariantsPattern

---
+++ app/DoctrineMigrations/Version20160224124124.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Furniture\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160224124124 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE product_variant ADD factory_code VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE product_variants_pattern ADD factory_code VARCHAR(255) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE product_variants_pattern DROP factory_code');
+        $this->addSql('ALTER TABLE product_variant DROP factory_code');
+    }
+}
