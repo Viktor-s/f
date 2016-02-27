b31be892fa06310a76607c36e24952aa62a6cb0c
Konstantin Shtompel <konstantin.shtompel@gmail.com>
2016-02-27 14:17:54 +0200

    fix issue

--- src/Furniture/ProductBundle/Form/Type/ProductPartFormType.php
+++ src/Furniture/ProductBundle/Form/Type/ProductPartFormType.php
@@ -9,6 +9,7 @@ use Furniture\ProductBundle\Entity\ProductPartType;
 use Furniture\ProductBundle\Entity\ProductPart;
 use Furniture\ProductBundle\Entity\ProductPartMaterial;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductPartFormType extends AbstractType
@@ -45,7 +46,11 @@ class ProductPartFormType extends AbstractType
     {
         $builder
             ->add('translations', 'a2lix_translationsForms', [
-                'form_type' => new ProductPartTranslationFormType()
+                'form_type' => new ProductPartTranslationFormType(),
+                // https://github.com/a2lix/TranslationFormBundle/issues/95
+                'empty_data' => function (FormInterface $form) {
+                    return new \Doctrine\Common\Collections\ArrayCollection();
+                },
             ])
             ->add('productPartMaterials', 'entity', [
                 'class' => ProductPartMaterial::class,
