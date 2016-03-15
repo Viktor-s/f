0902bd2c63fb993d745801aeed44a87774afa6e2
Konstantin Shtompel <konstantin.shtompel@gmail.com>
2016-03-15 18:19:03 +0200

    #1020 Add format_url filter

--- src/Furniture/FrontendBundle/Twig/FrontendExtension.php
+++ src/Furniture/FrontendBundle/Twig/FrontendExtension.php
@@ -36,7 +36,17 @@ class FrontendExtension extends \Twig_Extension
     public function getFunctions()
     {
         return [
-            'knp_menu_render_factory_side' => new \Twig_Function_Method($this, 'factorySideMenuRender', ['is_safe' => ['html']])
+            'knp_menu_render_factory_side' => new \Twig_Function_Method($this, 'factorySideMenuRender', ['is_safe' => ['html']]),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('format_url', [$this, 'urlFormatter']),
         ];
     }
 
@@ -56,10 +66,107 @@ class FrontendExtension extends \Twig_Extension
     }
 
     /**
+     * Format URL string
+     *
+     * @param       $url
+     * @param array $allowedParts
+     *
+     * @return string
+     */
+    public function urlFormatter($url, $parts = [])
+    {
+        $parts = empty($parts) ? ['scheme', 'host', 'path'] : $parts;
+
+        if ($parsedUrl = parse_url($url)) {
+            if ($formatted = $this->createFormatter($parts)) {
+                $url = $formatted;
+                foreach ($parsedUrl as $part => $value) {
+                    if (in_array($part, $parts)) {
+                        $this->replaceComponent($value, $part, $url);
+                    }
+                }
+            }
+        }
+
+        return $url;
+    }
+
+    /**
      * {@inheritDoc}
      */
     public function getName()
     {
         return 'furniture_frontend';
+    }
+
+    /**
+     * Helper function to create url formatter.
+     *
+     * @param $urlParts
+     *
+     * @return string
+     */
+    private function createFormatter($urlParts)
+    {
+        $url = '';
+        $single = count($urlParts) == 1;
+
+        if (in_array('scheme', $urlParts)) {
+            $url .= !$single ? ':scheme:://' : ':scheme:';
+        }
+
+        if (in_array('user', $urlParts)) {
+            $url .= ':user:';
+        }
+
+        if (in_array('pass', $urlParts)) {
+            if (!$single && in_array('user', $urlParts)) {
+                $url .= '::pass:';
+            }
+            else if ($single){
+                $url .= ':pass:';
+            }
+        }
+
+        if (!$single && in_array('user', $urlParts)) {
+            $url .= '@';
+        }
+        if (in_array('host', $urlParts)) {
+            $url .= ':host:';
+        }
+
+        if (in_array('port', $urlParts)) {
+            $url .= !$single ? '::port:' : ':port:';
+        }
+
+        if (in_array('path', $urlParts)) {
+            $url .= ':path:';
+        }
+
+        if (in_array('query', $urlParts)) {
+            $url .= !$single ? '?:query:' : ':query:';
+        }
+
+        if (in_array('fragment', $urlParts)) {
+            $url .= !$single ? '#:fragment:' : ':fragment:';
+        }
+
+        return $url;
+    }
+
+    /**
+     * Helper function to replace url formatter components.
+     *
+     * @param $value
+     * @param $component
+     * @param $url
+     *
+     * @return mixed
+     */
+    private function replaceComponent($value, $component, &$url)
+    {
+        $url = str_replace(":$component:", $value, $url);
+
+        return $url;
     }
 }
