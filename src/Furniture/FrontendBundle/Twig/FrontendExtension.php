26e70d47b10c5c173b37d0a6ca2242926231212a
Konstantin Shtompel <konstantin.shtompel@gmail.com>
2016-03-16 12:44:54 +0200

    Add Url formatter class

--- src/Furniture/FrontendBundle/Twig/FrontendExtension.php
+++ src/Furniture/FrontendBundle/Twig/FrontendExtension.php
@@ -5,6 +5,7 @@ namespace Furniture\FrontendBundle\Twig;
 use Furniture\FactoryBundle\Entity\Factory;
 use Furniture\FrontendBundle\Menu\FrontendMenuBuilder;
 use Knp\Menu\Twig\Helper as KnpMenuHelper;
+use Furniture\FrontendBundle\Util\UrlFormatter;
 
 class FrontendExtension extends \Twig_Extension
 {
@@ -36,7 +37,11 @@ class FrontendExtension extends \Twig_Extension
     public function getFunctions()
     {
         return [
-            'knp_menu_render_factory_side' => new \Twig_Function_Method($this, 'factorySideMenuRender', ['is_safe' => ['html']]),
+            'knp_menu_render_factory_side' => new \Twig_Function_Method(
+                $this,
+                'factorySideMenuRender',
+                ['is_safe' => ['html']]
+            ),
         ];
     }
 
@@ -69,26 +74,15 @@ class FrontendExtension extends \Twig_Extension
      * Format URL string
      *
      * @param       $url
-     * @param array $allowedParts
+     * @param array $parts
      *
      * @return string
      */
     public function urlFormatter($url, $parts = [])
     {
-        $parts = empty($parts) ? ['scheme', 'host', 'path'] : $parts;
+        $formatter = new UrlFormatter($url);
 
-        if ($parsedUrl = parse_url($url)) {
-            if ($formatted = $this->createFormatter($parts)) {
-                $url = $formatted;
-                foreach ($parsedUrl as $part => $value) {
-                    if (in_array($part, $parts)) {
-                        $this->replaceComponent($value, $part, $url);
-                    }
-                }
-            }
-        }
-
-        return $url;
+        return $formatter->format($parts);
     }
 
     /**
@@ -97,76 +91,5 @@ class FrontendExtension extends \Twig_Extension
     public function getName()
     {
         return 'furniture_frontend';
-    }
-
-    /**
-     * Helper function to create url formatter.
-     *
-     * @param $urlParts
-     *
-     * @return string
-     */
-    private function createFormatter($urlParts)
-    {
-        $url = '';
-        $single = count($urlParts) == 1;
-
-        if (in_array('scheme', $urlParts)) {
-            $url .= !$single ? ':scheme:://' : ':scheme:';
-        }
-
-        if (in_array('user', $urlParts)) {
-            $url .= ':user:';
-        }
-
-        if (in_array('pass', $urlParts)) {
-            if (!$single && in_array('user', $urlParts)) {
-                $url .= '::pass:';
-            }
-            else if ($single){
-                $url .= ':pass:';
-            }
-        }
-
-        if (!$single && in_array('user', $urlParts)) {
-            $url .= '@';
-        }
-        if (in_array('host', $urlParts)) {
-            $url .= ':host:';
-        }
-
-        if (in_array('port', $urlParts)) {
-            $url .= !$single ? '::port:' : ':port:';
-        }
-
-        if (in_array('path', $urlParts)) {
-            $url .= ':path:';
-        }
-
-        if (in_array('query', $urlParts)) {
-            $url .= !$single ? '?:query:' : ':query:';
-        }
-
-        if (in_array('fragment', $urlParts)) {
-            $url .= !$single ? '#:fragment:' : ':fragment:';
-        }
-
-        return $url;
-    }
-
-    /**
-     * Helper function to replace url formatter components.
-     *
-     * @param $value
-     * @param $component
-     * @param $url
-     *
-     * @return mixed
-     */
-    private function replaceComponent($value, $component, &$url)
-    {
-        $url = str_replace(":$component:", $value, $url);
-
-        return $url;
     }
 }
