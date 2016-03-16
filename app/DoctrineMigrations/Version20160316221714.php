301b3ebdf21ab26aa076d7623d3d72108a399efd
Konstantin Shtompel <konstantin.shtompel@gmail.com>
2016-03-16 22:27:20 +0200

    #1258 Make address field not required

---
+++ app/DoctrineMigrations/Version20160316221714.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Furniture\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160316221714 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE factory_contact ALTER address DROP NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $this->addSql('ALTER TABLE factory_contact ALTER address SET NOT NULL');
+    }
+}
