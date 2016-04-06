9b17bad8b74acf798f29e7d571cd632effdabd31
Konstantin Shtompel <konstantin.shtompel@gmail.com>
2016-04-06 15:56:13 +0300

    #2168 Product pattern create update options and parts requirements.

--- src/Furniture/ProductBundle/Entity/ProductVariantsPattern.php
+++ src/Furniture/ProductBundle/Entity/ProductVariantsPattern.php
@@ -4,10 +4,17 @@ namespace Furniture\ProductBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\PersistentCollection;
 use Furniture\ProductBundle\Entity\Pattern\AbstractProductVariantsPattern;
 use Furniture\SkuOptionBundle\Entity\SkuOptionVariant;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
+/**
+ * Class ProductVariantsPattern
+ * @package Furniture\ProductBundle\Entity
+ * @Assert\Callback(callback="validate")
+ */
 class ProductVariantsPattern extends AbstractProductVariantsPattern
 {
     /**
@@ -79,6 +86,50 @@ class ProductVariantsPattern extends AbstractProductVariantsPattern
     public function getActiveFactoryCode(){
         return $this->getFactoryCode() ? $this->getFactoryCode() : $this->getProduct()->getFactoryCode();
     }
-    
-    
+
+    /**
+     * Callback validation method.
+     */
+    public function validate(ExecutionContextInterface $context)
+    {
+        // Validate Product parts
+        $schemePartsCount = $this->getScheme()->getProductParts()->count();
+        if (!empty($schemePartsCount)) {
+            $selectionArray = [];
+            /** @var PersistentCollection $variantSelections */
+            $variantSelections = $this->getPartPatternVariantSelections();
+            /** @var PersistentCollection $schemeParts */
+            $schemeParts = $this->getScheme()->getProductParts();
+            /** @var ProductPartPatternVariantSelection $selection */
+            foreach ($variantSelections as $selection) {
+                if ($schemeParts->contains($selection->getProductPart())) {
+                    $selectionArray[$selection->getProductPart()->getId()] = true;
+                }
+            }
+
+            if ($schemePartsCount > count($selectionArray)) {
+                $context->buildViolation('You should select at least one variant for each parts')
+                    ->addViolation();
+            }
+        }
+
+        // Validate SKU options.
+        $productSkuOptions = $this->getProduct()->getSkuOptionVariantsGrouped();
+        if (!empty($productSkuOptions)) {
+            $selectionArray = [];
+            /** @var ArrayCollection $optionSelections */
+            $optionSelections = $this->getSkuOptionValues();
+            /** @var SkuOptionVariant $selection */
+            foreach ($optionSelections as $selection) {
+                if (array_key_exists($selection->getSkuOptionType()->getId(), $productSkuOptions)) {
+                    $selectionArray[$selection->getSkuOptionType()->getId()] = true;
+                }
+            }
+
+            if (count($productSkuOptions) > count($selectionArray)) {
+                $context->buildViolation('You should select at least one SKU option for each groups')
+                    ->addViolation();
+            }
+        }
+    }
 }
