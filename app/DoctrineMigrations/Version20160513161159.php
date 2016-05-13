b04543f1fb2cbe881552d3eb8c78b770ba094f24
Konstantin Shtompel <konstantin.shtompel@gmail.com>
2016-05-13 16:40:37 +0300

    Migration add graphJson field

---
+++ app/DoctrineMigrations/Version20160513161159.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Furniture\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160513161159 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE product_pdp_intellectual_root ADD graph_json JSON NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE product_pdp_intellectual_root DROP graph_json');
+    }
+}
